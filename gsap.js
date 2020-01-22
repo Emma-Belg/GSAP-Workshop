f179ff8fab6a88099e7a40d2f02247ef9ec016f0
Nicole Rey <nicolereyesgg@hotmail.com>
2020-01-22 09:42:33 +0100

    empty boilerplate

---
+++ gsap.js
@@ -0,0 +1,59 @@
+            /*1: ADVERTISEMENT*/
+
+
+
+            /*2: BOOKS + EYE ICON */
+
+
+// make timeline for books
+
+
+// decide the animation
+
+
+// select divs to be moved
+
+
+//add eventlisteners to it
+
+
+//open and close eye (as in javascript hills)
+
+
+            /*3: DUMMY TEXT: SCROLLMAGIC ANIMATION */
+
+
+// ANIMATION FOR DUMMY TEXT
+
+
+// INIT CONTROLLER
+
+
+// CREATE TRIGGER
+
+
+// CREATE SCENE : A Scene defines where the controller should react and how.
+
+
+//ADD SCENE TO CONTROLLER
+
+
+//ADD INDICATORS
+
+
+            /*4: MOTIONPATH SMILEY + CONTROLLING IT */
+
+
+//register the plugin (only once)
+
+
+//centers the smiley div on path
+
+
+//The animation: smiley travels through path
+
+
+// click handlers for controlling the tween instance...
+
+
+
